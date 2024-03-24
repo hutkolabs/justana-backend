9e9384edc353eb75db224b6a5f36f7829b98c1a0
Digberi <orkepuma@gmail.com>
2024-03-24 03:09:35 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as util from 'node:util';
+import { Logger } from '@nestjs/common';
 const log = console.log;
 
 console.log = (...args) => {
@@ -19,8 +20,12 @@ console.log = (...args) => {
   log(...inspectedArgs);
 };
 
+const PORT = 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT, () => {
+    Logger.log(`Server running on ${PORT} port`);
+  });
 }
 bootstrap();
